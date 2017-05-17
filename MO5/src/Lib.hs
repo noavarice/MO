b3f684b69c0be2e99406ca008480001a0a4668d2
noavarice <alex9razinkov@gmail.com>
2017-05-17 23:21:49 +0300

    fixed bug with wrong sign of gradient values

--- MO5/src/Lib.hs
+++ MO5/src/Lib.hs
@@ -8,25 +8,27 @@ someFunc :: IO ()
 someFunc =
         printf "%f, %f\n" (fst point) (snd point)
         where
-            point = fastestDescent (1.0, -3.0) 0.001 100
+            point = fastestDescent (103.0, 34.0) 0.000001 10000
 
-f x y = x ^ 2 + y ^ 2 - 4
+f x y = (x - 4) ^ 2 + (y + 7) ^ 2 - 4
 
-f_der_x x y = 2 * x
+f_der_x x y = 2 * x - 8
 
-f_der_y x y = 2 * y
+f_der_y x y = 2 * y + 14
 
-oppositeGrad x y = (-f_der_x x y, -f_der_y x y)
+gradient x y = (f_der_x x y, f_der_y x y)
 
 measure (x1, y1) (x2, y2) = sqrt $ (x1 - x2) ^ 2 + (y1 - y2) ^ 2
 
-step = 0.1
+step = 0.001
 
 fastestDescent :: (Double, Double) -> Double -> Integer -> (Double, Double)
-fastestDescent (x, y) eps iterCount
-        | iterCount == 0                    = (x, y)
-        | measure (x, y) nextPoint < eps    = nextPoint
-        | otherwise                         = fastestDescent nextPoint eps $ iterCount - 1
+fastestDescent startPoint eps iterCount
+        | iterCount == 0                        = startPoint
+        | measure startPoint nextPoint < eps    = nextPoint
+        | otherwise                             = fastestDescent nextPoint eps (iterCount - 1)
         where
-            oppGrad   = oppositeGrad x y
-            nextPoint = (x - step * fst oppGrad, y - step * snd oppGrad)
+            x = fst startPoint
+            y = snd startPoint
+            gradientValue= gradient x y
+            nextPoint = (x - step * fst gradientValue, y - step * snd gradientValue)
