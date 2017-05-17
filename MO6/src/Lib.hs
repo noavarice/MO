3d60ce8c4149a305c47509478340cd43274dfc40
noavarice <alex9razinkov@gmail.com>
2017-05-18 00:51:38 +0300

    first fckn compiled state

---
+++ MO6/src/Lib.hs
@@ -0,0 +1,46 @@
+module Lib
+    ( someFunc
+    ) where
+
+import Data.Bool
+import Text.Printf
+
+type Point = (Double, Double)
+
+someFunc :: IO ()
+someFunc
+        | snd result    = printf "Минимум найден в точке (%.4f, %.4f)\n" x y
+        | otherwise     = printf "Минимум не найден, последняя найденная точка: (%.4f, %.4f)\n" x y
+        where
+            result  = fastestDescent (103.0, 34.0) 0.000001 10000 0.01
+            (x, y) = fst result
+
+f (x, y) = (x - 4) ^ 2 + (y + 7) ^ 2 - 4
+
+f_der_x x y = 2 * x - 8
+
+f_der_y x y = 2 * y + 14
+
+gradient (x, y) = (f_der_x x y, f_der_y x y)
+
+measure (x1, y1) (x2, y2) = sqrt $ (x1 - x2) ^ 2 + (y1 - y2) ^ 2
+
+stepMultilplicator = 0.5;
+
+getNextPointAndStep currentPoint currentStep
+        | (f nextPoint) <= (f currentPoint) = (nextPoint, nextStep)
+        | otherwise                         = getNextPointAndStep currentPoint nextStep
+        where
+            x = fst currentPoint 
+            y = snd currentPoint
+            gradientValue = gradient (x, y)
+            nextPoint = (x - currentStep * fst gradientValue, y - currentStep * snd gradientValue)
+            nextStep = currentStep * stepMultilplicator 
+
+fastestDescent :: Point -> Double -> Integer -> Double -> (Point, Bool)
+fastestDescent startPoint eps iterCount step
+        | iterCount == 0                        = (startPoint, False)
+        | measure startPoint nextPoint < eps    = (nextPoint, True)
+        | otherwise                             = fastestDescent nextPoint eps (iterCount - 1) nextStep
+        where
+            (nextPoint, nextStep) = getNextPointAndStep startPoint step
